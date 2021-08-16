7d66de87e0a1aedb8f1a0830e351fadef0393159
takashicompany <t@kashi.company>
2021-08-16 16:19:24 +0900

    インターフェースを定義

---
+++ Unity/IVoxel.cs
@@ -0,0 +1,29 @@
+namespace TakashiCompany.Unity.VoxReader
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Linq;
+	using UnityEngine;
+
+	public interface IVoxel
+	{
+		Vector3Int voxelPosition { get; }
+		Vector3 positionFromCenter { get; }
+		HumanBodyBones bone { get; }
+
+		int x { get; }
+		int y { get; }
+		int z { get; }
+
+		void Init(Vector3Int position, Vector3 positionFromCenter, float unitPerSize, HumanBodyBones bone, int startIndex);
+
+		IEnumerable<Vector3> GetVertexPoints();
+
+		IEnumerable<int> GetTriangleIndices();
+	}
+
+	public static partial class Extension
+	{
+
+	}
+}
